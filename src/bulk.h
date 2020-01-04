718b76310e5eefb233da3b474c7d1509b6fa76fc
Andrey Shatalov <a.shatalov@rj-games.com>
2020-01-04 22:59:12 +0300

    main circle wip

--- src/bulk.h
+++ src/bulk.h
@@ -1,2 +1,32 @@
 #pragma once
 
+#include <iostream>
+
+class Bulk
+{
+    public:
+        Bulk(int commandBufCount)
+            : _commandBufCount(commandBufCount)
+        {
+        };
+
+        void Run()
+        {
+            std::cout<<__PRETTY_FUNCTION__<<std::endl;
+            while (true)
+            {
+                std::cout<<"Waiting for input:"<<std::endl;
+                std::string command;
+                std::cin>>command;
+                if (std::cin.eof())
+                {
+                    break;
+                }
+                std::cout<<"Input is: " <<command<<std::endl;
+            }
+            std::cout<<"Input complete aborting"<<std::endl;
+        };
+
+    private:
+        int _commandBufCount;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include "bulk.h"
 
 //! Main app function
-int main(int, char const *[])
+int main(int, char const *argv[])
 {
+    Bulk bulk{atoi(argv[1])};
+    bulk.Run();
     //todo
     return 0;
 }
